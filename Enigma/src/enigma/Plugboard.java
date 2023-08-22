3fc1151b93265be4a8e306ad10aeedc25419a4d8
Daniel <23375128+Daniel1060@users.noreply.github.com>
2023-08-22 23:34:03 +0100

    Added Plugboard Backend

---
+++ Enigma/src/enigma/Plugboard.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package enigma;
+
+import java.util.*;
+
+/**
+ *
+ * @author Daniel
+ */
+class Plugboard {
+    private Map<Character, Character> connections = new HashMap();
+    
+    
+    // Input letters get added to hashmap as keys with their opposite as values
+    public void addConnection(char letter1, char letter2){
+        if(connections.containsKey(letter1)){
+            System.out.println(letter1 +" and "+letter2 +" are already connected!");
+            return;
+        }
+        connections.put(letter1, letter2);
+        connections.put(letter2, letter1);
+    }
+    
+    // Checks if input is connected to another value and if so returns that value, otherwise returns original input
+    public char encrypt(char input){
+        return connections.getOrDefault(input, input);
+    }
+}
