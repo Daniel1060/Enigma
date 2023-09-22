ef58cc6127d50801c41e766472ff6974b2bfb5df
Daniel O'Hara <d.ohara@rgu.ac.uk>
2023-09-22 18:53:48 +0100

    Added Rotor functionality Further testing required

---
+++ Enigma/src/enigma/Rotor.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package enigma;
+
+/**
+ *
+ * @author Daniel
+ */
+class Rotor {
+    private char[] wiring;
+    private int position;
+    private char[] notch;
+    private char[] turnover;
+
+    public Rotor(char[] wiring, int position) {
+        this.wiring = wiring;
+        this.position = position;
+    }
+    
+    public void setInputPosition(char inputLetter){
+        position = inputLetter -'A';
+    }
+    public void setOutputPosition(char outputLetter){
+        int desiredPosition = outputLetter -'A';
+        
+        for (int i=0; i<wiring.length; i++){
+            if(wiring[i]==desiredPosition){
+                position =i;
+                break;
+            }
+        }
+    }
+    
+    public char getCurrentLetter(){
+        return (char) (position+'A');
+    }
+    
+    public char encryptForward(char input){
+        return (char)(wiring[input-'A']);
+    }
+    public char encryptBackward(char input){
+        for(int i=0; i<wiring.length;i++){
+            if(wiring[i]==input){
+                return(char)(i+'A');
+            }
+        }
+        return '?';
+    }
+    public void rotate(){
+        if(position == wiring.length-1){
+            position=0;
+        }else{
+            position++;
+        }
+    }
+}
